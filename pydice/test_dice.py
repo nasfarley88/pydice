40caa98a1e8482f4655eded8ccbec1fc76586519
Nathanael Farley <nasfarley88@gmail.com>
2017-07-05 12:49:17 +0100

    Initial changes to use hypothesis testing.

--- pydice/test_dice.py
+++ pydice/test_dice.py
@@ -1,11 +1,14 @@
 import unittest
 import random
 import six
+from hypothesis import given, strategies as st
+import functools
 
 if six.PY2:
     import dice
 else:
     from .import dice
+
 
 # all hail stochastic testing
 CASE_ITERATIONS = 10000
@@ -22,11 +25,9 @@ def random_size():
     return random.randint(1, MAX_DIE_SIZE+1)
 
 
+@functools.lru_cache()
 def faces_from_size(x_size=MAX_DIE_SIZE):
-    if six.PY3:
-        return list(range(1, x_size+1))
-    else:
-        return range(1, x_size+1)
+    return range(1, x_size+1)
 
 
 def random_faces(x_size=MAX_DIE_SIZE):
@@ -57,33 +58,31 @@ class CheckRollNDX(unittest.TestCase):
     Verifies the roll_ndx() function generates a Roll with the appropriate
     dice.
     """
-    def test(self):
-        for x in range(CASE_ITERATIONS):
-            n_dice = random_ndice()
-            x_size = random_size()
-            faces = faces_from_size(x_size)
+    @given(n_dice=st.sampled_from(range(1, 20)), x_size=st.sampled_from(range(2, 10000)))
+    def test(self, n_dice, x_size):
+        faces = faces_from_size(x_size)
 
-            r = dice.roll_ndx(n_dice, x_size)
+        r = dice.roll_ndx(n_dice, x_size)
 
-            # number of Dice in Roll should be n_dice
-            self.assertTrue(len(r.dice) == n_dice)
+        # number of Dice in Roll should be n_dice
+        self.assertTrue(len(r.dice) == n_dice)
 
-            for d in r.dice:
-                # each Die in Roll should have a result in faces
-                self.assertTrue(d.result in faces,
-                                'roll_ndx({n}, {x}) contains {d} result {r} not in faces {f}'\
-                                .format(n=n_dice, x=x_size, d=d, r=d.result, f=faces))
+        for d in r.dice:
+            # each Die in Roll should have a result in faces
+            self.assertTrue(d.result in faces,
+                            'roll_ndx({n}, {x}) contains {d} result {r} not in faces {f}'\
+                            .format(n=n_dice, x=x_size, d=d, r=d.result, f=faces))
 
-            # overall result should be between n_dice and n_dice * x_size
-            lower_limit = n_dice
-            self.assertTrue(r.total >= lower_limit,
-                            'roll_ndx({n}, {x}) yielded result {r} with total {t} less than {l}'\
-                            .format(n=n_dice, x=x_size, r=r.result, t=r.total, l=lower_limit))
+        # overall result should be between n_dice and n_dice * x_size
+        lower_limit = n_dice
+        self.assertTrue(r.total >= lower_limit,
+                        'roll_ndx({n}, {x}) yielded result {r} with total {t} less than {l}'\
+                        .format(n=n_dice, x=x_size, r=r.result, t=r.total, l=lower_limit))
 
-            upper_limit = n_dice * x_size
-            self.assertTrue(r.total <= upper_limit,
-                            'roll_ndx({n}, {x}) yielded total {t} greater than {l}'\
-                            .format(n=n_dice, x=x_size, r=r.result, t=r.total, l=upper_limit))
+        upper_limit = n_dice * x_size
+        self.assertTrue(r.total <= upper_limit,
+                        'roll_ndx({n}, {x}) yielded total {t} greater than {l}'\
+                        .format(n=n_dice, x=x_size, r=r.result, t=r.total, l=upper_limit))
 
 
 class NDXRollString(unittest.TestCase):
@@ -155,7 +154,7 @@ class NDXwmodRollString(unittest.TestCase):
 
             for d in r.dice:
                 # each Die in Roll should have a result in faces or faces*mod
-                faces_w_mod = faces + [f+mod for f in faces]
+                faces_w_mod = set(faces) | set(f+mod for f in faces)
                 self.assertTrue(d.result in faces,
                                 'Parsed {s} contains {d} result {r} not in faces(+mod) {f}'\
                                 .format(s=test_string, d=d, r=d.result, f=faces_w_mod))
