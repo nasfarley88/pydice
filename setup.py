d9dfb9a7c9d27644a24120487eb9d53be69b0378
Nathanael Farley <nasfarley88@gmail.com>
2017-07-05 11:59:50 +0100

    CircleCI (#2)
    * Added circle test yml and nose2.
    
    * Added two python versions to circle.yml.
    
    * Added multiple python versions checking.
    
    * Added running tests to tox.
    
    * Neatened requirements.

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         #'dev': ['check-manifest'],
-        #'test': ['coverage'],
+        'test': ['nose2'],
     },
 
     # If there are data files included in your packages that need to be
