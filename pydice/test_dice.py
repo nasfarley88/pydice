55170cc9fa411a9672da9a77c71d561139445fd6
Nathanael Farley <nasfarley88@gmail.com>
2017-07-05 13:29:20 +0100

    Converted completely to hypothesis testing.

--- pydice/test_dice.py
+++ pydice/test_dice.py
@@ -1,7 +1,6 @@
 import unittest
-import random
 import six
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, strategies as st
 import functools
 
 if six.PY2:
@@ -17,25 +16,15 @@ MAX_N_DICE = 20
 
 
 # helpers for randomization
-def random_ndice(upper_limit=MAX_N_DICE):
-    return random.randint(1, upper_limit) + 1
-
-
-def random_size():
-    return random.randint(1, MAX_DIE_SIZE+1)
-
-
 @functools.lru_cache()
 def faces_from_size(x_size=MAX_DIE_SIZE):
     return range(1, x_size+1)
 
 
-def random_faces(x_size=MAX_DIE_SIZE):
-    return faces_from_size(random.randint(2, x_size+1))
-
-
-def random_mod(limit=MAX_DIE_SIZE):
-    return random.randint(1, limit+1)
+# Strategies
+@st.composite
+def random_faces(draw, elements=st.integers(min_value=2, max_value=MAX_DIE_SIZE)):
+    return faces_from_size(draw(elements))
 
 
 # cases
@@ -44,13 +33,13 @@ class DieResultInFaces(unittest.TestCase):
     A Die object should always return values in
     its die faces, specified at init.
     """
-
-    def test(self):
-        for x in range(CASE_ITERATIONS):
-            f = random_faces()
-            d = dice.Die(faces=f)
-            d.roll()
-            self.assertTrue(d.result in f)
+    # If it takes longer than 1 sec to make a Die object, this is unacceptable
+    @settings(timeout=1)
+    @given(f=random_faces())
+    def test(self, f):
+        d = dice.Die(faces=f)
+        d.roll()
+        self.assertTrue(d.result in f)
 
 
 class CheckRollNDX(unittest.TestCase):
@@ -59,6 +48,7 @@ class CheckRollNDX(unittest.TestCase):
     dice.
     """
     @given(n_dice=st.sampled_from(range(1, 20)), x_size=st.sampled_from(range(2, 10000)))
+    @settings(timeout=10)
     def test(self, n_dice, x_size):
         faces = faces_from_size(x_size)
 
@@ -93,34 +83,36 @@ class NDXRollString(unittest.TestCase):
     pattern = '{n}d{x}'
 
     # testing ndx
-    def test(self):
-        for x in range(CASE_ITERATIONS):
-            n_dice = random_ndice()
-            x_size = random_size()
-            faces = faces_from_size(x_size)
+    @given(n_dice=st.sampled_from(range(1, 20)), x_size=st.sampled_from(range(2, 10000)))
+    @settings(timeout=10)
+    def test(self, n_dice, x_size):
+        # for x in range(CASE_ITERATIONS):
+        # n_dice = random_ndice()
+        # x_size = random_size()
+        faces = faces_from_size(x_size)
 
-            test_string = self.pattern.format(n=n_dice, x=x_size)
-            r = dice.roll(test_string)
+        test_string = self.pattern.format(n=n_dice, x=x_size)
+        r = dice.roll(test_string)
 
-            # number of Dice in Roll should be n_dice
-            self.assertTrue(len(r.dice) == n_dice)
+        # number of Dice in Roll should be n_dice
+        self.assertTrue(len(r.dice) == n_dice)
 
-            for d in r.dice:
-                # each Die in Roll should have a result in faces
-                self.assertTrue(d.result in faces,
-                                'Parsed {s} contains {d} result {r} not in faces {f}'\
-                                .format(s=test_string, d=d, r=d.result, f=faces))
+        for d in r.dice:
+            # each Die in Roll should have a result in faces
+            self.assertTrue(d.result in faces,
+                            'Parsed {s} contains {d} result {r} not in faces {f}'\
+                            .format(s=test_string, d=d, r=d.result, f=faces))
 
-            # overall result should be between n_dice and n_dice * x_size
-            lower_limit = n_dice
-            self.assertTrue(r.total >= lower_limit,
-                            'Parsed {s} yielded result {r} with total {t} less than {l}'\
-                            .format(s=test_string, r=r.result, t=r.total, l=lower_limit))
+        # overall result should be between n_dice and n_dice * x_size
+        lower_limit = n_dice
+        self.assertTrue(r.total >= lower_limit,
+                        'Parsed {s} yielded result {r} with total {t} less than {l}'\
+                        .format(s=test_string, r=r.result, t=r.total, l=lower_limit))
 
-            upper_limit = n_dice * x_size
-            self.assertTrue(r.total <= upper_limit,
-                            'Parsed {s} yielded total {t} greater than {l}'\
-                            .format(s=test_string, r=r.result, t=r.total, l=upper_limit))
+        upper_limit = n_dice * x_size
+        self.assertTrue(r.total <= upper_limit,
+                        'Parsed {s} yielded total {t} greater than {l}'\
+                        .format(s=test_string, r=r.result, t=r.total, l=upper_limit))
 
 
 class NDXwmodRollString(unittest.TestCase):
@@ -132,6 +124,7 @@ class NDXwmodRollString(unittest.TestCase):
     # testing ndx
     # TODO create hypothesis strategy for making dice expressions
     @given(n_dice=st.sampled_from(range(1, 20)), x_size=st.sampled_from(range(2, 10000)), mod=st.integers())
+    @settings(timeout=10)
     def test(self, n_dice, x_size, mod):
         faces = faces_from_size(x_size)
 
